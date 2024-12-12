bb96b6f2a1311b7a8e6bf12d89e7cad569323c2f
paxfeline <paxfeline@gmail.com>
2024-12-12 00:12:22 -0800

    fixed big part of riff_kind bug

--- app/models/riff.rb
+++ app/models/riff.rb
@@ -8,8 +8,8 @@ class Riff < ApplicationRecord
 
     attribute :riff_kind, :integer
 
-    after_initialize :update_riff_kind
-    after_touch :update_riff_kind
+    #after_initialize :update_riff_kind
+    #after_touch :update_riff_kind
 
     # not needed?:
     #def riff_kind
@@ -17,23 +17,35 @@ class Riff < ApplicationRecord
     #    self[:isText] ? (self[:speak] ? 2 : 3) : 1
     #end
     
-    def update_riff_kind
-        self[:riff_kind] = self[:isText] ? (self[:speak] ? 2 : 3) : 1
-    end
+    #def update_riff_kind
+        #self[:riff_kind] = self[:isText] ? (self[:speak] ? 2 : 3) : 1
+        #self.riff_kind = self.isText ? (self.speak ? 2 : 3) : 1
+    #end
     
     def riff_kind=(value)
-        self.inspect
-        if value == 1
-            self[:isText] = false
-            self[:speak] = false
-        elsif value == 2
-            self[:isText] = true
-            self[:speak] = true
-        elsif value == 3
-            self[:isText] = true
-            self[:speak] = false
+        #puts "in"
+        #puts self.inspect
+        #debugger
+        if value.to_i == 1
+            #self[:isText] = false
+            #self[:speak] = false
+            self.isText = false
+            self.speak = false
+        elsif value.to_i == 2
+            #self[:isText] = true
+            #self[:speak] = true
+            self.isText = true
+            self.speak = true
+        elsif value.to_i == 3
+            #self[:isText] = true
+            #self[:speak] = false
+            self.isText = true
+            self.speak = false
         end
-        update_riff_kind
+        #update_riff_kind
+        super(value.to_i)
+        #puts "out"
+        #puts self.inspect
     end
       
 end
